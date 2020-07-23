5ec3293303caf8436fab763459d175b662e99f9c
VijayaP-Cuelogic <vijaya.panchal@cuelogic.com>
2020-07-23 21:11:39 +0530

    commit changes

--- app/src/main/java/com/example/oopsconceptskotlin/JavaActivity.java
+++ app/src/main/java/com/example/oopsconceptskotlin/JavaActivity.java
@@ -1,7 +1,10 @@
 package com.example.oopsconceptskotlin;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class JavaActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class JavaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
+
+        Intent myIntent = getIntent(); // this is just for example purpose
+        final String keyIdentifier = myIntent.getStringExtra("keyIdentifier");
+        System.out.println(keyIdentifier);
     }
 }
--- app/src/main/java/com/example/oopsconceptskotlin/SecondFragment.kt
+++ app/src/main/java/com/example/oopsconceptskotlin/SecondFragment.kt
@@ -1,5 +1,6 @@
 package com.example.oopsconceptskotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_java.*
+import kotlinx.android.synthetic.main.activity_java.view.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -25,7 +28,18 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+           // findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            Intent(this, JavaActivity::class.java)
         }
     }
+
+    private fun Intent(secondFragment: SecondFragment, java: Class<JavaActivity>) {
+        val Intent = Intent(this.activity, JavaActivity::class.java)
+        // To pass any data to next activity
+        Intent.putExtra("keyIdentifier", "Anjali")
+       // start your next activity
+        startActivity(Intent)
+
+    }
 }
+
