3527ad8854ceaca46bd9ac09f47cf4c9750b0cfe
VijayaP-Cuelogic <vijaya.panchal@cuelogic.com>
2020-07-24 22:22:23 +0530

    Android UI creation using XML

---
+++ app/src/main/java/com/example/oopsconceptskotlin/ActivitySecond.kt
@@ -0,0 +1,12 @@
+package com.example.oopsconceptskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySecond : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
