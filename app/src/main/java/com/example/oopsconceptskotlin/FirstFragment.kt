fae4d6cc49553cc1f99afe3510ec18bb1605d94a
VijayaP-Cuelogic <vijaya.panchal@cuelogic.com>
2020-07-27 17:21:45 +0530

    commit changes

--- app/src/main/java/com/example/oopsconceptskotlin/FirstFragment.kt
+++ app/src/main/java/com/example/oopsconceptskotlin/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.example.oopsconceptskotlin
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_first.*
 //import sun.jvm.hotspot.utilities.IntArray
@@ -15,10 +18,10 @@ import kotlinx.android.synthetic.main.fragment_first.*
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-    var b1 = button
+    var loginButton = button
     var b2 = button2
-    var ed1 = editText
-    var ed2 = editText2
+    var userName = editText
+    var password = editText2
     var txt1 = textView
 //    var text2 = textView2
 //    var text3 = textView3
@@ -33,12 +36,17 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        val button = button2
         view.findViewById<Button>(R.id.button).setOnClickListener {
             // findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
             Intent(secondFragment=this, java=ActivitySecond::class.java)
         }
 
+        view.findViewById<Button>(R.id.button2).setOnClickListener {
+            // findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            val context = getContext()
+            Toast.makeText(context,"Show Toast",Toast.LENGTH_SHORT).show()
+
+        }
     }
 
     private fun Intent(secondFragment: FirstFragment, java: Class<ActivitySecond>) {
@@ -46,7 +54,9 @@ class FirstFragment : Fragment() {
         // To pass any data to next activity
        // Intent.putExtra("keyIdentifier", "Anjali")
         // start your next activity
-        startActivity(Intent)
+            val context = getContext()
+            Toast.makeText(context,"Redirecting",Toast.LENGTH_SHORT).show()
+            startActivity(Intent)
 
     }
 }
