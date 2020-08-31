34cb5e2d88e95d7824acee1fafd772e5225f930f
VijayaP-Cuelogic <65525929+VijayaP-Cuelogic@users.noreply.github.com>
2020-08-31 16:01:12 +0530

    Update ActivityDeviceDetails.kt
    wearable activity changes

--- app/src/main/java/com/example/cuelogicinventoryassignment/ActivityDeviceDetails.kt
+++ app/src/main/java/com/example/cuelogicinventoryassignment/ActivityDeviceDetails.kt
@@ -1,15 +1,11 @@
 package com.example.cuelogicinventoryassignment
 
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
 
-class ActivityDeviceDetails : WearableActivity() {
+class ActivityDeviceDetails : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_device_details)
-
-        // Enables Always-on
-        setAmbientEnabled()
     }
 }
